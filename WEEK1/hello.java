d6c183f53a1fee6a96967c3a51ef9d2cc284541f
Yogesh Chaudhary <chaudharyyogesh258@gmail.com>
2024-04-14 12:41:32 +0545

    task1-4

--- WEEK1/hello.java
+++ WEEK1/hello.java
@@ -1,7 +1,76 @@
 package WEEK1;
 //Write a program that prints Hello World! to the console. Save the program as Hello.java.
-public class hello {
-    public static void main(String[] args){
-        System.out.println("Hello World!");
-    }
-}
+// public class hello {
+//     public static void main(String[] args){
+//         System.out.println("Hello World!");
+//     }
+// }
+
+
+// 2. Extend/Modify the above program to print two additional lines:
+
+// This is my first program
+
+// I am on module ST4003CEM
+
+// System.out.print (not println) and produces the same output.
+
+// public class hello{
+//     public static void main(String[] args){
+//         System.out.print("This is my first program!\n");
+
+//         System.out.print("I am on module ST4003CEM");
+//     }
+// }
+
+// 3. Write a program that prints the following output:
+
+// A "quoted" String is
+
+// 'much' better if you learn
+
+// the rules of "escape sequences."
+
+// Also, "" represents an empty
+
+// String. Don't forget: use \"
+
+// instead of " !
+
+// '' is not the same as "
+
+// public class hello{
+//     public static void main(String[] args) {
+//         System.out.println("A \"quoted\" String is");
+//         System.out.println("\'much\' better if you learn");
+//         System.out.println("the rules of \"escape sequences.\"");
+//         System.out.println("Also, \"\" represents an empty ");
+//         System.out.println("String. Don't forget: use \\\"");
+//         System.out.println("instead of \"!");
+//         System.out.println("\'\'is not the same as");
+//     }
+
+// }
+
+// 4. WAP that prints the following pattern:
+
+// *
+
+// **
+
+// ***
+
+
+// ****
+
+
+// public class hello{
+//     public static void main(String[] args) {
+//         System.out.println("*");
+//         System.out.println("* *");
+//         System.out.println("* * *");
+//         System.out.println("* * * *");
+//         System.out.println("* * * * *");
+//     }
+// }
+

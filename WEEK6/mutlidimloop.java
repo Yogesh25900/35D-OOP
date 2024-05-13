31658eaaa19a16c757ca9a2b0e83c5d7591d8eb6
Yogesh Chaudhary <chaudharyyogesh258@gmail.com>
2024-05-13 13:49:43 +0545

    Week6

--- WEEK6/mutlidimloop.java
+++ WEEK6/mutlidimloop.java
@@ -6,12 +6,25 @@ public class mutlidimloop {
                             {-10,-20},
                             {100,200},
                             {-100,-200}};
-    int outerlength = newArray.length;
-    for(int outer = 0; outer < outerlength; outer++){
-        for(int index = 0; index < newArray[outer].length; index++){
-            System.out.println("INdex [" + outer+ "][" + index+ "] :" + newArray[outer][index]);
+    // int outerlength = newArray.length;
+    // for(int outer = 0; outer < outerlength; outer++){
+    //     for(int index = 0; index < newArray[outer].length; index++){
+    //         System.out.println("Index [" + outer+ "][" + index+ "] :" + newArray[outer][index]);
+
+    // }
+    // }
+    //make an array of string names
+    String[][] names = {
+                        {"Sanskriti","Nikita","Anji"},
+                        {"Miraj","Niraj","Oscar"}
+    };
+    for(int outer = 0; outer < names.length; outer++){
+        for(int index = 0; index < names[outer].length; index++){
+            System.out.println("Index [" + outer+ "][" + index+ "] :" +names[outer][index]);
 
     }
     }
+   
+    
 }
 }
